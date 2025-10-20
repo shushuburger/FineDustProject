9bc5ab2db74522f1aa0d94e58930876d3f900b1a
shushuburger <03robin@naver.com>
2025-10-21 05:26:44 +0900

    fix.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/FineDustProject/', // GitHub Pages를 위한 base path
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
