21ef7fab7fda6d3705ca77fba34c4211ad712c83
shushuburger <03robin@naver.com>
2025-10-21 05:01:15 +0900

    fix: console error 해결

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/FineDustProject/', // GitHub Pages를 위한 base path 설정
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
