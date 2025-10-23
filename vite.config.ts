7a803f2a875430cea132f71577402b5b1068515f
shushuburger <03robin@naver.com>
2025-10-23 16:28:22 +0900

    .

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
